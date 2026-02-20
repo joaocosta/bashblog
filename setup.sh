544b94f9df3951e798a67dedcd01fd8d9c734406
João Costa <joaocosta@zonalivre.org>
2026-02-20 00:50:07 +0000

    chore: include submodule initialization in setup.sh
    Improve developer onboarding by adding 'git submodule update --init'
    to the setup script. This allows a single command to configure both
    Git hooks and the testing framework.

--- setup.sh
+++ setup.sh
@@ -6,5 +6,8 @@ set -euo pipefail
 printf "Setting up Git hooks...\n"
 git config core.hooksPath hooks
 
+printf "Initializing submodules...\n"
+git submodule update --init --recursive
+
 printf "\nSetup complete! You can now run tests with:\n"
 printf "./tests/bats-core/bin/bats tests/bb.bats\n"
